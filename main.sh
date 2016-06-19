9cb0cebdc046e776f6f43f901dbd58d99d8ed557
Evan Klitzke <evan@eklitzke.org>
2016-06-19 09:37:44 -0700

    no more c

--- main.sh
+++ main.sh
@@ -6,6 +6,7 @@ LD_LIBRARY_PATH=$HOME/local/lib
 
 # this is a 64-bit system
 declare -i wordsize=8
+declare -i PY_FILE_INPUT=257
 declare -r ZERO=int:0
 
 script=$(cat<<EOF
@@ -14,36 +15,12 @@ def add(*args):
 EOF
 )
 
-cat<<EOF >python.c
-#include <assert.h>
-
-#include <Python.h>
-
-// XXX: leaks references
-PyObject *CreateFunction(const char *funcname, const char *code) {
-  // compile a code object
-  PyObject *compiled = Py_CompileString(code, "", Py_file_input);
-  assert(compiled != NULL);
-
-  // build a module w/ the code object
-  PyObject *module = PyImport_ExecCodeModule((char *)funcname, compiled);
-  assert(module != NULL);
-
-  // get the function we created
-  PyObject *method = PyObject_GetAttrString(module, (char *)funcname);
-  assert(method != NULL);
-
-  return method;
+function ensure_not_null {
+    if [ "$1" = $NULL ]; then
+        echo "null result"
+        exit 1
+    fi
 }
-EOF
-
-function build_python {
-    sofile=$(mktemp /tmp/XXXXXX.so)
-    cc $(pkg-config --cflags --libs python) -fPIC -shared python.c -o $sofile 2>/dev/null
-    echo $sofile
-}
-
-sofile=$(build_python)
 
 trap "rm -f $sofile python.c" EXIT
 
@@ -56,7 +33,7 @@ dlcall -n buffer -r pointer malloc $((numwords * wordsize))
 pack $buffer words
 
 # load the code
-dlopen $sofile
+dlopen libpython2.7.so
 
 # initialize the python interpreter
 dlcall Py_Initialize
@@ -66,10 +43,7 @@ dlcall -n pytuple -r pointer PyTuple_New long:$numwords
 i=0
 for word in "${words[@]}"; do
     dlcall -n s -r pointer PyString_FromString $word
-    if [ $s = $NULL ]; then
-        echo "failed to PyString_FromString"
-        exit 1
-    fi
+    ensure_not_null $s
     dlcall -n r -r int PyTuple_SetItem $pytuple long:$i $s
     if [ $r != $ZERO ]; then
         echo "failed to PyTuple_SetItem"
@@ -78,8 +52,14 @@ for word in "${words[@]}"; do
     i=$((i + 1))
 done
 
-# create a python function object for "add"
-dlcall -n pyfunc -r pointer CreateFunction string:add string:"$script"
+dlcall -n pycompiled -r pointer Py_CompileString string:"$script" "" $PY_FILE_INPUT
+ensure_not_null $pycompiled
+
+dlcall -n pymodule -r pointer PyImport_ExecCodeModule string:add $pycompiled
+ensure_not_null $module
+
+dlcall -n pyfunc -r pointer PyObject_GetAttrString $pymodule string:add
+ensure_not_null $pyfunc
 
 # call the function
 dlcall -n out -r pointer PyObject_CallObject $pyfunc $pytuple
