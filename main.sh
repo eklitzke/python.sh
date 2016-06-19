d0078501daa08b3d438401608dcd6aac1d121c84
Evan Klitzke <evan@eklitzke.org>
2016-06-19 09:51:28 -0700

    implement the miller-rabin primality test

--- main.sh
+++ main.sh
@@ -11,8 +11,37 @@ declare -r ZERO=int:0
 declare funcname="string:add"
 
 script=$(cat<<EOF
+import random
+import sys
+
 def add(*args):
     return sum(int(arg) for arg in args)
+
+def is_probable_prime(n, k = 7):
+   """use Rabin-Miller algorithm to return True (n is probably prime)
+      or False (n is definitely composite)"""
+   if n < 6:  # assuming n >= 0 in all cases... shortcut small cases here
+      return [False, False, True, True, False, True][n]
+   elif n & 1 == 0:  # should be faster than n % 2
+      return False
+   else:
+      s, d = 0, n - 1
+      while d & 1 == 0:
+         s, d = s + 1, d >> 1
+      # Use random.randint(2, n-2) for very large numbers
+      for a in random.sample(xrange(2, min(n - 2, sys.maxint)), min(n - 4, k)):
+         x = pow(a, d, n)
+         if x != 1 and x + 1 != n:
+            for r in xrange(1, s):
+               x = pow(x, 2, n)
+               if x == 1:
+                  return False  # composite for sure
+               elif x == n - 1:
+                  a = 0  # so we know loop didn't continue to end
+                  break  # could be strong liar, try another a
+            if a:
+               return False  # composite if we reached end of this loop
+      return True  # probably prime if reached end of outer loop
 EOF
 )
 
@@ -40,6 +69,7 @@ dlopen libpython2.7.so
 dlcall Py_Initialize
 
 dlcall -n pytuple -r pointer PyTuple_New long:$numwords
+ensure_not_null $pytuple
 
 i=0
 for word in "${words[@]}"; do
@@ -61,14 +91,40 @@ ensure_not_null $pycompiled
 dlcall -n pymodule -r pointer PyImport_ExecCodeModule $funcname $pycompiled
 ensure_not_null $module
 
-# get the function from the module
-dlcall -n pyfunc -r pointer PyObject_GetAttrString $pymodule $funcname
-ensure_not_null $pyfunc
+# get the add function from the module
+dlcall -n addfunc -r pointer PyObject_GetAttrString $pymodule $funcname
+ensure_not_null $addfunc
 
-# call the function
-dlcall -n out -r pointer PyObject_CallObject $pyfunc $pytuple
+# call the add function
+dlcall -n out -r pointer PyObject_CallObject $addfunc $pytuple
 ensure_not_null $out
 
 # unmarshal the return value
 dlcall -n res -r long PyInt_AsLong $out
-printf "return value is %d\n" $(echo $res | egrep -o '[0-9]+')
+printf "add: return value is %d\n" $(echo $res | egrep -o '[0-9]+')
+
+# get the is_probable_prime function from the module
+dlcall -n millerrabin -r pointer PyObject_GetAttrString $pymodule string:is_probable_prime
+ensure_not_null $millerrabin
+
+for x in {2..100}; do
+    dlcall -n pytuple -r pointer PyTuple_New long:1
+    ensure_not_null $pytuple
+
+    dlcall -n num -r pointer PyInt_FromLong long:$x
+    ensure_not_null $num
+
+    dlcall -n r -r int PyTuple_SetItem $pytuple long:0 $num
+    if [ $r != $ZERO ]; then
+        echo "failed to PyTuple_SetItem"
+        exit 1
+    fi
+
+    # call the add function
+    dlcall -n out -r pointer PyObject_CallObject $millerrabin $pytuple
+    ensure_not_null $out
+
+    # unmarshal the return value
+    dlcall -n res -r long PyInt_AsLong $out
+    printf "is_probable_prime: $x -> %d\n" $(echo $res | egrep -o '[0-9]+')
+done
